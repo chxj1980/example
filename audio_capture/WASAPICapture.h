4732436967904817b588f1c30c30787f70670436
PHZ76 <2235710879@qq.com>
2019-08-12 23:37:29 -0700

    add wasapi capture

--- audio_capture/WASAPICapture.h
+++ audio_capture/WASAPICapture.h
@@ -11,12 +11,11 @@
 #include <mutex>
 #include <thread>
 
-#define REFTIMES_PER_SEC  10000000
-#define REFTIMES_PER_MILLISEC  10000
-
 class WASAPICapture
 {
 public:
+	typedef std::function<void(WAVEFORMATEX *m_mixFormat, BYTE *data, uint32_t samples)> PacketCallback;
+
 	WASAPICapture();
 	~WASAPICapture();
 	WASAPICapture &operator=(const WASAPICapture &) = delete;
@@ -26,15 +25,26 @@ public:
 	int exit();
 	int start();
 	int stop();
+	void setCallback(PacketCallback callback);
 
 private:
+	int capture();
+
 	const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
 	const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 	const IID IID_IAudioClient = __uuidof(IAudioClient);
 	const IID IID_IAudioCaptureClient = __uuidof(IAudioCaptureClient);
+	const int REFTIMES_PER_SEC = 10000000;
+	const int REFTIMES_PER_MILLISEC = 10000;
 
 	bool m_initialized;
+	bool m_isEnabeld;
+	std::mutex m_mutex, m_mutex2;
+	std::shared_ptr<std::thread> m_threadPtr;
 	WAVEFORMATEX *m_mixFormat;
+	REFERENCE_TIME m_hnsActualDuration;
+	uint32_t m_bufferFrameCount;
+	PacketCallback m_callback;
 	Microsoft::WRL::ComPtr<IMMDeviceEnumerator> m_enumerator;
 	Microsoft::WRL::ComPtr<IMMDevice> m_device;
 	Microsoft::WRL::ComPtr<IAudioClient> m_audioClient;
--- audio_capture/main.cpp
+++ audio_capture/main.cpp
@@ -3,11 +3,24 @@
 
 extern int test_opus_codec();
 
+void dataCB(WAVEFORMATEX *m_mixFormat, BYTE *data, uint32_t samples)
+{
+	printf("audio samples: %d\n", samples);
+}
+
 int main(int argc, char **argv)
 {
 	WASAPICapture audioCapture;
 	audioCapture.init();
+	audioCapture.setCallback(dataCB);
+	audioCapture.start();
 
+	while (1)
+	{
+		Sleep(1000);
+	}
+
+	audioCapture.stop();
 	getchar();
 	return 0;
 }
